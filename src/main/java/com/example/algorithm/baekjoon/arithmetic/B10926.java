43c5124842bb437b8bbff457c2f871fac100cc2d
김성훈 <sunghoon@gimseonghun-ui-MacBookPro.local>
2022-09-09 21:43:03 +0900

    10926번

---
+++ src/main/java/com/example/algorithm/baekjoon/arithmetic/B10926.java
@@ -0,0 +1,16 @@
+package com.example.algorithm.baekjoon.arithmetic;
+
+import java.util.*;
+
+class B10926 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+
+        System.out.println(name + "??!" );
+
+
+    }
+}
